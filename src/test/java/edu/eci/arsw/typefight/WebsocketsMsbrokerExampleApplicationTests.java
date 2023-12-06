2c3188f196beae0e34f06f704637f64126d59626
AndresOnate <andres.onate@mail.escuelaing.edu.co>
2023-12-06 16:49:03 -0500

    Redis New Key

--- src/test/java/edu/eci/arsw/typefight/WebsocketsMsbrokerExampleApplicationTests.java
+++ src/test/java/edu/eci/arsw/typefight/WebsocketsMsbrokerExampleApplicationTests.java
@@ -1,20 +1,45 @@
 package edu.eci.arsw.typefight;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import edu.eci.arsw.typefight.model.Player;
+import edu.eci.arsw.typefight.model.TypeFight;
+
 import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class WebsocketsMsbrokerExampleApplicationTests {
 
+	private Player player;
+    private TypeFight typeFight;
+
+	@Before
+    public void setUp() {
+        player = new Player("John", "Rojo");
+        typeFight = new TypeFight();
+    }
+
 	@Test
 	public void contextLoads() {
 		assertEquals(1,1);
 	}
+
+    @Test
+    public void testAddPlayer() {
+        typeFight.addPlayer(player);
+        List<Player> players = typeFight.getSortedPlayers();
+        List<Player> actualPlayers = new ArrayList<>();
+        actualPlayers.add(player);
+        assertEquals(players, actualPlayers);
+    }
 
 
 }
